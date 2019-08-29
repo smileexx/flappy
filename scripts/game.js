b7e7f88d505f8782f5db79a9f1db7e65509aacdb
smileexx <smilee900work@gmail.com>
2019-08-30 00:34:06 +0300

    Add gravity

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,7 @@
 Gcanvas = null;
 Gctx = null;
+G_GRAVITY = 7;
+G_VELOCITY = 2;
 window.onload = function () {
     window.Gcanvas = document.getElementById('canvas');
     window.Gctx = Gcanvas.getContext('2d');
@@ -10,6 +12,13 @@ window.onload = function () {
 
 function Game() {
 
+    /**
+     * @type Pipe
+     */
+    let pipe = null;
+    /**
+     * @type Bird
+     */
     let bird = null;
     let pipes = [];
 
@@ -17,16 +26,25 @@ function Game() {
         console.log('Game started');
 
         // init all staff
-
+        pipe = new Pipe(Gcanvas.width / 2);
         bird = new Bird(50, 50);
+
+        // run loop
+        update();
     };
 
 
     function update() {
-        // do some action
+        // draw background
+        Gctx.fillStyle = "#4ed0f5";
+        Gctx.fillRect(0, 0, Gcanvas.width, Gcanvas.height);
 
         // Loop game and redraw canvas
-        // requestAnimationFrame(update);
+
+        bird.move(G_GRAVITY);
+        pipe.move(G_VELOCITY);
+
+        requestAnimationFrame(update);
     }
 
 };
@@ -37,8 +55,24 @@ function Game() {
  * Pipe Class
  * @constructor
  */
-function Pipe() {
+function Pipe(x, y) {
+    let self = this;
+    this.x = x || 0;
+    this.y = y || 0;
 
+
+    this.move = function (velocity) {
+        this.x -= velocity;
+
+        draw();
+    };
+
+    function draw() {
+        Gctx.fillStyle = "green";
+        Gctx.fillRect(self.x, self.y, 100, 150);
+    }
+
+    draw();
 };
 
 
@@ -49,10 +83,25 @@ function Pipe() {
  */
 function Bird(x, y) {
     let size = 20;
-    Gctx.beginPath();
-    Gctx.arc(x, y, size, 0, Math.PI * 2);
-    Gctx.fill();
+    let self = this;
 
+    this.x = x;
+    this.y = y;
+
+    this.move = function (gravity) {
+        this.y += gravity;
+
+        draw();
+    };
+
+    function draw() {
+        Gctx.beginPath();
+        Gctx.fillStyle = "yellow";
+        Gctx.arc(self.x, self.y, size, 0, Math.PI * 2);
+        Gctx.fill();
+    }
+
+    draw();
 };
 
 // ###################################################################################################################
